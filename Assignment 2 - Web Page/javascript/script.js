3e67085faeee174bab609c21d854d9011d801fa6
aniketbidgar3 <aniketbidgar3@gmail.com>
2024-02-24 02:20:15 +0530

    "Enable Download , Dark Mode , Etc Functions"

--- Assignment 2 - Web Page/javascript/script.js
+++ Assignment 2 - Web Page/javascript/script.js
@@ -1,14 +1,20 @@
+function previewTitle() {
+  updatePreview();
+  getValue();
+}
+
 function updatePreview() {
-  var title = $("#title").val(); 
   var text = document.getElementById("textInput").value;
-  text = title + "\n\n" + text;
   text = text.replace(/\n/g, "<br>");
   var previewSection = document.getElementById("previewSection");
   previewSection.innerHTML = text;
 }
 
-window.onload = updatePreview;
-
+function getValue() {
+  var inputElement = document.getElementById("title");
+  var value = inputElement.value;
+  document.getElementById("previewTitle").innerText = value;
+}
 function lowercaseText() {
   var text = document.getElementById("textInput").value;
   document.getElementById("textInput").value = text.toLowerCase();
@@ -28,7 +34,6 @@ function clearText() {
     document.getElementById("title").value = "";
     updatePreview();
   }
- 
 }
 
 function copyText() {
@@ -54,4 +59,21 @@ function capitalize() {
   updatePreview();
 }
 
+function applyFont() {
+  var selectedFont = document.getElementById("fonts").value;
+  document.getElementById("textInput").style.fontFamily = selectedFont;
+  document.getElementById("title").style.fontFamily = selectedFont;
+  document.getElementById("previewSection").style.fontFamily = selectedFont;
+  document.getElementById("previewTitle").style.fontFamily = selectedFont;
+}
 
+function darkMode() {
+  var mode = document.getElementById("mode").innerHTML;
+  if (mode == '<i class="fa fa-moon-o"></i> ') {
+    document.body.classList.add("dark-mode");
+    document.getElementById("mode").innerHTML = '<i class="fa fa-sun-o"></i> ';
+  } else {
+    document.body.classList.remove("dark-mode");
+    document.getElementById("mode").innerHTML = '<i class="fa fa-moon-o"></i> ';
+  }
+}
